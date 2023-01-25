84bbe953ac769b58e00ed8d9e6e47239adc6ad58
Joel-Nogueira <joelnogueira733@gmail.com>
2023-01-25 08:51:25 -0300

    Crud de Estados e Cidades

--- src/App.tsx
+++ src/App.tsx
@@ -11,6 +11,14 @@ import UsuarioListagem from './view/usuario/listagem';
 import UsuarioCadastro from './view/usuario/cadastro';
 import UsuarioExclusao from './view/usuario/exclusao';
 
+import EstadoListagem from './view/estado/listagem';
+import EstadoCadastro from './view/estado/cadastro';
+import EstadoExclusao from './view/estado/exclusao';
+
+import CidadeListagem from './view/cidade/listagem';
+import CidadeCadastro from './view/cidade/cadastro';
+import CidadeExclusao from './view/cidade/exclusao';
+
 function App() {
   return (
     <Provider store={store}>
@@ -29,6 +37,20 @@ function App() {
             <Route path='/usuario/editar/:id' component={UsuarioCadastro} />
             <Route path='/usuario/visualizar/:id/:action' component={UsuarioCadastro} />
             <Route path='/usuario/exclusao/:id' component={UsuarioExclusao} />
+
+            <Route exact path='/estado' component={EstadoListagem} />
+            <Route path='/estado/pagina/:pagina' component={EstadoListagem} />
+            <Route exact path='/estado/novo' component={EstadoCadastro} />
+            <Route path='/estado/editar/:id' component={EstadoCadastro} />
+            <Route path='/estado/visualizar/:id/:action' component={EstadoCadastro} />
+            <Route path='/estado/exclusao/:id' component={EstadoExclusao} />
+
+            <Route exact path='/cidade' component={CidadeListagem} />
+            <Route path='/cidade/pagina/:pagina' component={CidadeListagem} />
+            <Route exact path='/cidade/novo' component={CidadeCadastro} />
+            <Route path='/cidade/editar/:id' component={CidadeCadastro} />
+            <Route path='/cidade/visualizar/:id/:action' component={CidadeCadastro} />
+            <Route path='/cidade/exclusao/:id' component={CidadeExclusao} />
 
             <Route path='*' component={PageNotFound} />
           </Switch>
